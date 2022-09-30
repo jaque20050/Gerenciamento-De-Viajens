de7afc70856505d5800a9535b335f816ee338624
jaque20050 <jaque20050@hotmail.com>
2022-09-30 15:07:54 -0300

    Iniciando a criação do pacotes

---
+++ src/main/java/com/github/jaque20050/travelsjavaapi/TravelsJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.jaque20050.travelsjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelsJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jaque20050/travelsjavaapi/TravelsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jaque20050.travelsjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
